7429498862f1ef38dd42aca63575066c5d06d661
Karim <85011758+ked-source@users.noreply.github.com>
2024-03-02 20:37:22 +0400

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", (event)=>{
+    let neededitem = document.getElementById('Layer_3');
+
+    neededitem.addEventListener("click", ()=>{document.getElementById("textbox").innerText = "This is an image of a pepsi can made in Adobe Illustrator."});
+});
