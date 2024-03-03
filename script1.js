0c2763b62a45904d3fde8405602a0fc194e2448e
Karim <85011758+ked-source@users.noreply.github.com>
2024-03-03 14:13:43 +0400

    Delete script1.js

--- script1.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", (event)=>{
-    let neededitem = document.getElementById('Layer_3');
-
-    neededitem.addEventListener("click", ()=>{document.getElementById("textbox").innerText = "This is an image of a pepsi can made in Adobe Illustrator."});
-});
