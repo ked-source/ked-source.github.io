0269d6e4e6a35cf04d39ff0638ecc233ed593f9f
Karim <85011758+ked-source@users.noreply.github.com>
2024-04-29 00:32:06 +0400

    Add files via upload

--- VideoProj/script.js
+++ VideoProj/script.js
@@ -6,3 +6,21 @@ function openOverlay() {
     document.getElementById('overlay').style.display = 'none';
   }
   
+
+  window.addEventListener("DOMContentLoaded", (event)=>{
+    let neededitem = document.getElementById('Erfan');
+
+    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+});
+
+window.addEventListener("DOMContentLoaded", (event)=>{
+    let neededitem = document.getElementById('Francisca');
+
+    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+});
+
+window.addEventListener("DOMContentLoaded", (event)=>{
+    let neededitem = document.getElementById('Momo');
+
+    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+});
