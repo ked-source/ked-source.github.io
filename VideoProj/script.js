eecb76d6c6074a408cbd6ae3ffadb86a9f3b8fdf
Karim <85011758+ked-source@users.noreply.github.com>
2024-04-28 07:02:43 +0400

    Add files via upload

---
+++ VideoProj/script.js
@@ -0,0 +1,8 @@
+function openOverlay() {
+    document.getElementById('overlay').style.display = 'block';
+  }
+  
+  function closeOverlay() {
+    document.getElementById('overlay').style.display = 'none';
+  }
+  
