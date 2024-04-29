08a825a7065c5a18a8c78265ade551a4d0de1176
Karim <85011758+ked-source@users.noreply.github.com>
2024-04-29 08:09:09 +0400

    Add files via upload

--- VideoProj/script.js
+++ VideoProj/script.js
@@ -10,17 +10,23 @@ function openOverlay() {
   window.addEventListener("DOMContentLoaded", (event)=>{
     let neededitem = document.getElementById('Erfan');
 
-    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+    neededitem.addEventListener("click", ()=>{
+        document.getElementById("player").src = "https://drive.google.com/file/d/1Wz9m-QCPkDnt7v-9ErkWaObvsCB7PyKS/preview";
+        document.getElementById('overlay').style.display = 'block';});
 });
 
 window.addEventListener("DOMContentLoaded", (event)=>{
     let neededitem = document.getElementById('Francisca');
 
-    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+    neededitem.addEventListener("click", ()=>{
+        document.getElementById("player").src = "https://drive.google.com/file/d/1uEP3er8PiaJ24ljoblwLaBaYnoZcJwSj/preview";
+        document.getElementById('overlay').style.display = 'block';});
 });
 
 window.addEventListener("DOMContentLoaded", (event)=>{
     let neededitem = document.getElementById('Momo');
-
-    neededitem.addEventListener("click", ()=>{document.getElementById('overlay').style.display = 'block';});
+    
+    neededitem.addEventListener("click", ()=>{
+        document.getElementById("player").src = "https://drive.google.com/file/d/1hA_96bfeCl4SYfd89hFbPTKLFGEMn0CT/preview";
+        document.getElementById('overlay').style.display = 'block';});
 });
